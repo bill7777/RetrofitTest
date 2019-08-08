4b241881aee37a7065d787a51dce4f8b3a4ce142
bill7777 <bill@bankpro.com.tw>
2019-08-08 10:29:19 +0800

    new model

---
+++ app/src/main/java/tw/com/test/retrofitdemo/models/Suda7.kt
@@ -0,0 +1,12 @@
+package tw.com.test.retrofitdemo.models
+
+import com.google.gson.annotations.SerializedName
+
+class Suda7 {
+    @SerializedName("Code")
+    var Code: String? = null
+    @SerializedName("Message")
+    var Message: String? = null
+    @SerializedName("Data")
+    var Data: String? = null
+}
