2b46f6ce9b68afc99ce4d87a41c2252daa9381c2
FirstSnow77 <e1236699@gmail.com>
2019-08-08 09:45:01 +0800

    update AppClientManager

--- app/src/main/java/tw/com/test/retrofitdemo/AppClientManager.kt
+++ app/src/main/java/tw/com/test/retrofitdemo/AppClientManager.kt
@@ -1,36 +1,34 @@
 package tw.com.test.retrofitdemo
 
-import android.util.Log
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+class AppClientManager constructor(url: String) {
+    private var url: String = url
 
-class AppClientManager private constructor() {
-    private val retrofit: Retrofit
-    private var logging = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
-        override fun log(message: String) {
-            Log.i("interceptor msg", message)
-        }
-    })
+//    private var logging = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+//        override fun log(message: String) {
+//            Log.i("interceptor msg", message)
+//        }
+//    })
 
-    private var okHttpClient : OkHttpClient
+//    private var okHttpClient : OkHttpClient
 
-    init {
-        logging.level = HttpLoggingInterceptor.Level.BODY
-        okHttpClient = OkHttpClient().newBuilder().addInterceptor(logging).build()
-        retrofit = Retrofit.Builder()
-                .baseUrl(Config.URL)
+//    init {
+//        logging.level = HttpLoggingInterceptor.Level.BODY
+//        okHttpClient = OkHttpClient().newBuilder().addInterceptor(logging).build()
+//        retrofit = Retrofit.Builder()
+//                .baseUrl(Config.URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .client(okHttpClient)
+//                .build()
+//    }
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(this.url)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
                 .build()
-    }
-
-    companion object {
-        private val manager = AppClientManager()
-        val client: Retrofit
-            get() = manager.retrofit
     }
 }
 
