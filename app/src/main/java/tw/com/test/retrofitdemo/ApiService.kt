2bc9b379ca8f90c65946a643c594178cdd3c465e
FirstSnow77 <e1236699@gmail.com>
2019-08-08 09:29:08 +0800

    update ApiService

--- app/src/main/java/tw/com/test/retrofitdemo/ApiService.kt
+++ app/src/main/java/tw/com/test/retrofitdemo/ApiService.kt
@@ -2,8 +2,10 @@ package tw.com.test.retrofitdemo
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
+import tw.com.test.retrofitdemo.models.Suda7
 
 interface ApiService {
-    @GET("/posts")
-    fun index(): Call<List<Posts>>
+    @GET("api/tds/AskSuda7v2")
+    fun suda7(@Query("CustID") CustID: String, @Query("CustAddress") CustAddress: String, @Query("GetSuda7_dashv2") GetSuda7_dashv2: String): Call<Suda7>
 }
